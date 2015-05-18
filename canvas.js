d65bddf3856a978c5b1dfd895f539227d87d99fd
Chris Lee <sihrc.c.lee@gmail.com>
2015-05-18 19:36:08 -0400

    ADD: undo and redo capability

--- canvas.js
+++ canvas.js
@@ -2,17 +2,55 @@
 var clickX = new Array();
 var clickY = new Array();
 var clickDrag = new Array();
+
+// Undo
+var cPushArray = new Array();
+var cStep = -1;
+
+// Canvas
+var canvas;
 var paint;
 
+// Colors
+var COLOR_DEFAULT = "#000F55";
+
+
+// Setting the image to be used as background
 var image = document.createElement('img');
-image.src = "./samples/29.jpg";
+image.src = "https://slm-assets3.secondlife.com/assets/7071830/lightbox/greeting_card_balloons.jpg?1359617623";
 image.onload = function () {
     init(image.width, image.height);
     context.drawImage(image, 0, 0);
 }
 
-// Colors
-var COLOR_DEFAULT = "#000F55";
+// Setting up buttons
+$("#save").click(function (e) {
+    console.log("TODO");
+});
+
+$("#undo").click(function (e) {
+    if (cStep > 0) {
+        cStep--;
+        var canvasPic = new Image();
+        canvasPic.src = cPushArray[cStep];
+        canvasPic.onload = function () { ctx.drawImage(canvasPic, 0, 0); }
+    }
+});
+
+$("#redo").click(function (e) {
+    if (cStep < cPushArray.length-1) {
+        cStep++;
+        var canvasPic = new Image();
+        canvasPic.src = cPushArray[cStep];
+        canvasPic.onload = function () { ctx.drawImage(canvasPic, 0, 0); }
+    }
+});
+
+// Undo / Redo functinality
+var saveStep = function() {
+    cPushArray.push(canvas.toDataURL());
+    cStep++;
+}
 
 function addClick(x, y, dragging) {
     clickX.push(x);
@@ -71,6 +109,7 @@ function setupClickListeners() {
 
     var mousecancel = function(e) {
         paint = false;
+        saveStep();
     };
 
     $canvas.mousedown(mousedown);
@@ -89,7 +128,7 @@ function setupClickListeners() {
 // Prepare for HTML5 Canvas Scripting
 function init(canvasWidth, canvasHeight) {
     var canvasDiv = document.getElementById('draw-card-canvas');
-    var canvas = document.createElement('canvas');
+    canvas = document.createElement('canvas');
 
     // Set Canvas Attributes
     canvas.setAttribute('width', canvasWidth);

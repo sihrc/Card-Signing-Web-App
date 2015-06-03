13f1fba36240d5df3c7b380cc6e8a5eb5ff31fe7
Chris Lee <sihrc.c.lee@gmail.com>
2015-06-02 21:18:39 -0400

    FIX: Resizing image to be screen width

--- canvas.js
+++ canvas.js
@@ -1,5 +1,10 @@
 var signaturePad;
 var canvas;
+
+var previousOrientation;
+var viewPortWidth;
+var viewPortHeight;
+
 var undoStack = [];
 var redoStack = [];
 
@@ -11,11 +16,20 @@ function previewFile() {
     var image = document.createElement('img');
     image.src = reader.result;
     image.setAttribute('crossOrigin', 'anonymous');
+
     image.onload = function () {
       if (canvas)
         document.getElementById('draw-card-canvas').removeChild(canvas);
-      init(image.width, image.height);
-      context.drawImage(image, 0, 0);
+
+      var scaleX = viewPortWidth / image.width;
+      var scaleY = viewPortHeight / image.height;
+      var scaleFactor = scaleX > scaleY ? scaleY : scaleX;
+
+      var newWidth = image.width * scaleFactor;
+      var newHeight = image.height * scaleFactor;
+
+      init(newWidth, newHeight);
+      context.drawImage(image, 0, 0, newWidth, newHeight);
       undoStack.push(signaturePad.toDataURL());
     };
   }
@@ -88,8 +102,47 @@ function init(canvasWidth, canvasHeight) {
     }
   };
 }
+function getViewport() {
+  // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+  if (typeof window.innerWidth != 'undefined') {
+    viewPortWidth = window.innerWidth,
+      viewPortHeight = window.innerHeight
+  }
 
+  // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
+  else if (typeof document.documentElement != 'undefined' && typeof document.documentElement.clientWidth !=
+    'undefined' && document.documentElement.clientWidth != 0) {
+    viewPortWidth = document.documentElement.clientWidth,
+      viewPortHeight = document.documentElement.clientHeight
+  }
+
+  // older versions of IE
+  else {
+    viewPortWidth = document.getElementsByTagName('body')[0].clientWidth,
+      viewPortHeight = document.getElementsByTagName('body')[0].clientHeight
+  }
+  return [viewPortWidth, viewPortHeight];
+}
+
+var checkOrientation = function(){
+    if(window.orientation !== previousOrientation){
+        previousOrientation = window.orientation;
+        // orientation changed, do your magic here
+        getViewport();
+    }
+};
+
+
+// (optional) Android doesn't always fire orientationChange on 180 degree turns
+setInterval(checkOrientation, 2000);
 // On Document Load initialize everything
 $(document).ready(function () {
   setupButtons();
+  getViewport();
+
+  previousOrientation = window.orientation;
+
+  window.addEventListener("resize", checkOrientation, false);
+  window.addEventListener("orientationchange", checkOrientation, false);
+
 });
